2b06f61aad9a72e1e8627df316f7a460ff693af4
michaelmirhom <micharby73@gmail.com>
2023-04-02 12:01:07 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers,string){
+    return drivers.filter((driver)=>driver.toLowerCase
+    ()===string.toLowerCase());
+}
+function  fuzzyMatch(drivers,string){
+return drivers.filter((driver)=>driver.slice(0,2)===string.slice(0,2));
+}
+function  matchName(driver,string){
+    return driver.filter((driver)=>driver.name===string);
+}
+
